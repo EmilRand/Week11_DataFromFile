2eed0f2d2b1690c3b092c1a7adc8f5cc4189e2d7
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:26:42 +0200

    DataFromFile

---
+++ Chuck/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+namespace Chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directorypath = @"C:\Users\opilane\samples";
+            string FileName = "chuck.txt";
+            string FullPath = $@"{directorypath}\{FileName}";
+            
+            string[] dataFromFile = File.ReadAllLines(FullPath);
+            Random rnd = new Random();
+            Console.WriteLine(dataFromFile[rnd.Next(dataFromFile.Length)]);
+
+        }
+    }
+}
---
+++ DataFromFile/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wagwan, mis su lemmik värv on, blood?");
+            string FavColour = Console.ReadLine();
+
+            string directorypath = @"C:\Users\opilane\samples";
+            string FileName = "personality.txt";
+            string FullPath = $@"{directorypath}\{FileName}";
+            string[] dataFromFile = File.ReadAllLines(FullPath);
+
+
+            private static string GetLineFromFileData(string userImput)
+            {
+                string[] dataFromFile = ReadDataFromFile();
+                string result = "";
+                foreach (string line in dataFromFile) ;
+            }
+
+            string[] dataFromfile = File.ReadAllLines(FullPath);
+
+           
+            
+            
+            foreach (string line in dataFromfile) 
+            {
+                if (line.Contains(FavColour))
+                {
+                    Console.WriteLine(line) ;
+                    break;                
+                }
+            }
+
+        }
+    }  
+}
